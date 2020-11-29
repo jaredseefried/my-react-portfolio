5db2023cda382e45dba94f884ee695eb2cc82256
Jared Seefried <jared.seefried@yahoo.com>
2020-11-29 15:23:29 -0700

    fixing contact page

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -27,6 +27,14 @@ function Contact() {
             </div>
           </div>
         </div>
+        <div className="contact-form">
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text" id="inputGroup-sizing-default">Default</span>
+            </div>
+            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+          </div>
+        </div>
       </div>
 
     </div>
