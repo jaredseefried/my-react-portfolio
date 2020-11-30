6daa25d76c0fad61e5b88f0664492c5fc23297fa
Jared Seefried <jared.seefried@yahoo.com>
2020-11-29 21:37:23 -0700

    Added README. Updated File Structure. Created Server.js. Installed Mongo and Mongoose. Created Routes Folders and api-routes file.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+const Contact = require("./model/contact-form");
 const express = require("express");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const Contact = require("../model/contact-form");
+const Contact = require("./model/contact-form");
 const app = express();
 
 
@@ -19,6 +20,16 @@ const options = {
   useFindAndModify: false
 }
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/portfolioContactForm", options);
+
+app.post("/submit", ({ body }, res) => {
+  Contact.create(body)
+    .then(contactSubmit => {
+      res.json(contactSubmit);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -37,8 +37,8 @@ function Contact() {
                   <div className="input-group-prepend">
                     <span className="input-group-text">Name</span>
                   </div>
-                    <input type="text" name="firstName" placeholder="First Name" aria-label="First name" class="form-control" />
-                    <input type="text" name="lastName" placeholder="Last Name" aria-label="Last name" class="form-control" />
+                    <input type="text" name="firstName" placeholder="First Name" aria-label="First name" className="form-control" />
+                    <input type="text" name="lastName" placeholder="Last Name" aria-label="Last name" className="form-control" />
                   </div>
                 </div>
                 <div className="input-group mb-3">
@@ -47,11 +47,11 @@ function Contact() {
                   </div>
                   <input type="text" placeholder="Email" name="email" className="form-control" autofocus/>
                 </div>
-                <div class="input-group">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">Subject</span>
+                <div className="input-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">Subject</span>
                   </div>
-                  <textarea class="form-control" name="subject" placeholder="Enter a subject" aria-label="With textarea"></textarea>
+                  <textarea className="form-control" name="subject" placeholder="Enter a subject" aria-label="With textarea"></textarea>
                 </div>
               </div>
               <button type="submit" className="btn btn-primary submit-btn">Submit</button>
