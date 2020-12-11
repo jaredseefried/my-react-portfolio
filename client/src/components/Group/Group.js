f99ae02f2db85c0eb75aa7f15128a71f1c99746e
Jared Seefried <jared.seefried@yahoo.com>
2020-12-11 10:10:58 -0700

    Added pictures and links to portfolio page

--- client/src/components/Group/Group.js
+++ client/src/components/Group/Group.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import pandemicTracker from '../../images/pandemic-tracker.jpg'
+import shelfIndulgence from '../../images/shelf-indulgence.jpg'
+import mealMatcher from '../../images/meal-matcher.jpg'
+import './group.css'
+
 
 function Group() {
   return (
@@ -9,31 +14,28 @@ function Group() {
       <div className="row col-12">
         <div className="col-4 card-div">
           <div className="card portfolio-card">
-            <img className="card-img-top" src="https://d9qtyz43wogdd.cloudfront.net/pixpacom/images/homebanner-image.jpg" alt="" />
+            <img className="card-img-top" src={pandemicTracker} alt="" />
             <div className="card-body body-card">
-              <h5 className="card-title">Front End Development</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
+              <h5 className="card-title port-card-title">Pandemic Tracker</h5>
+              <a href="https://my-pandemic-tracker.herokuapp.com/" target="_blank" className="card-text port-card-text">A MERN (Mongo, Express, React and Node) Application using React-Globe and API's to show updated stats on Covid-19. </a>          
             </div>
           </div>
         </div>
         <div className="col-4 card-div">
           <div className="card portfolio-card">
-            <img className="card-img-top" src="https://d9qtyz43wogdd.cloudfront.net/pixpacom/images/homebanner-image.jpg" alt="" />
+            <img className="card-img-top" src={shelfIndulgence} alt="" />
             <div className="card-body body-card">
-              <h5 className="card-title">Front End Development</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
-            </div>
+              <h5 className="card-title port-card-title">Shelf Indulgence</h5>
+              <p className="card-text port-card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
           </div>
         </div>
         <div className="col-4 card-div">
           <div className="card portfolio-card align-middle">
-            <img className="card-img-top" src="https://d9qtyz43wogdd.cloudfront.net/pixpacom/images/homebanner-image.jpg" alt="" />
+            <img className="card-img-top" src={mealMatcher} alt="" />
             <div className="card-body body-card">
-              <h5 className="card-title">Front End Development</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-
+              <h5 className="card-title port-card-title">Meal Matcher</h5>
+              <p className="card-text port-card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
         </div>
--- client/src/components/Individual/Individual.js
+++ client/src/components/Individual/Individual.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import '../Group/group.css'
+import chatApp from '../../images/chat-app.jpg'
+import empDir from '../../images/employee-directory.jpg'
+import burger from '../../images/eat-da-burger.jpg'
+import weather from '../../images/weather-dashboard.jpg'
+
 
 function Individual() {
   return (
@@ -10,40 +16,40 @@ function Individual() {
         <div className="row cards">
           <div className="col-3 card-div">
             <div className="card portfolio-card align-middle">
-              <img className="card-img-top" src="https://d9qtyz43wogdd.cloudfront.net/pixpacom/images/homebanner-image.jpg" alt="" />
+              <img className="card-img-top" src={chatApp} alt="" />
               <div className="card-body body-card">
-                <h5 class="card-title">Front End Development</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 className="card-title port-card-title">Chat Application</h5>
+                <a href="https://my-socketio-chat-app.herokuapp.com/" target="_blank" className="card-text port-card-text">A real time chat application using Express and Socket.io</a>
 
               </div>
             </div>
           </div>
           <div className="col-3 card-div">
             <div className="card portfolio-card align-middle">
-              <img className="card-img-top" src="https://d9qtyz43wogdd.cloudfront.net/pixpacom/images/homebanner-image.jpg" alt="" />
+              <img className="card-img-top" src={empDir} alt="" />
               <div className="card-body body-card">
-                <h5 class="card-title">Front End Development</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 className="card-title port-card-title">Employee Directory</h5>
+                <a href="https://my-employee-directory-react.herokuapp.com/" target="_blank" className="card-text port-card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</a>
 
               </div>
             </div>
           </div>
           <div className="col-3 card-div">
             <div className="card portfolio-card align-middle">
-              <img className="card-img-top" src="https://d9qtyz43wogdd.cloudfront.net/pixpacom/images/homebanner-image.jpg" alt="" />
+              <img className="card-img-top" src={burger} alt="" />
               <div className="card-body body-card">
-                <h5 class="card-title">Front End Development</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 className="card-title port-card-title">Eat-Da-Burger</h5>
+                <a href="https://east-da-burger-mvc.herokuapp.com/" target="_blank" className="card-text port-card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</a>
 
               </div>
             </div>
           </div>
           <div className="col-3 card-div">
             <div className="card portfolio-card align-middle">
-              <img className="card-img-top" src="https://d9qtyz43wogdd.cloudfront.net/pixpacom/images/homebanner-image.jpg" alt="" />
+              <img className="card-img-top" src={weather} alt="" />
               <div className="card-body body-card">
-                <h5 class="card-title">Front End Development</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 className="card-title port-card-title">Weather Dashboard</h5>
+                <a href="https://jaredseefried.github.io/weather-dashboard/" target="_blank" className="card-text port-card-text">The dashboard calls a third party API (openweathermap.com) </a>
 
               </div>
             </div>
